c9d61e3dd5e76d4ca108bda3652a96711d6cc875
MartinPettersen <searchingtheskyfor@gmail.com>
2025-03-26 14:05:58 +0100

    setup

---
+++ sanity.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+
+if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) {
+    throw new Error("Missing NEXT_PUBLIC_SANITY_PROJECT_ID environment variable");
+}
+
+const config = defineConfig({
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+    title: "Roy's Portfolio Website",
+    apiVersion: "2025-03-04",
+    basePath: "/admin",
+    plugins: [structureTool()]
+});
+export default config
