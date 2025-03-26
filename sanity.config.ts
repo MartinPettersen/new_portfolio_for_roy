0318084611770a9c9ac74d6b494dbea3adfad3bf
MartinPettersen <searchingtheskyfor@gmail.com>
2025-03-26 14:23:34 +0100

    add project schema

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
+import project from './sanity/schemas/project-schema';
 
 if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) {
     throw new Error("Missing NEXT_PUBLIC_SANITY_PROJECT_ID environment variable");
@@ -11,6 +12,7 @@ const config = defineConfig({
     title: "Roy's Portfolio Website",
     apiVersion: "2025-03-04",
     basePath: "/admin",
-    plugins: [structureTool()]
+    plugins: [structureTool()],
+    schema: { types: [project]}
 });
 export default config
