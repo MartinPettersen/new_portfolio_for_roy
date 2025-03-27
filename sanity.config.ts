875e989184880d7656a69d0220c951e357586367
MartinPettersen <searchingtheskyfor@gmail.com>
2025-03-27 11:50:31 +0100

    add barrel file for schemas

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
-import project from './sanity/schemas/project-schema';
-import contentBlock from './sanity/schemas/contentBlock-schema';
-import hobby from './sanity/schemas/hobby-schema';
-import about from './sanity/schemas/about-schema';
+import schemas from './sanity/schemas';
+
 
 if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) {
     throw new Error("Missing NEXT_PUBLIC_SANITY_PROJECT_ID environment variable");
@@ -16,6 +14,6 @@ const config = defineConfig({
     apiVersion: "2025-03-04",
     basePath: "/admin",
     plugins: [structureTool()],
-    schema: { types: [project, contentBlock, hobby, about]}
+    schema: { types: schemas}
 });
 export default config
---
+++ sanity/schemas/index.ts
@@ -0,0 +1,9 @@
+import project from './project-schema';
+import contentBlock from './contentBlock-schema';
+import hobby from './hobby-schema';
+import about from './about-schema';
+
+const schemas = [project, contentBlock, hobby, about]
+
+
+export default schemas
