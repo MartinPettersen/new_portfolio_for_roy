592f5a07eb8dc99d0e6ed33cc50696295beaf8c2
MartinPettersen <searchingtheskyfor@gmail.com>
2025-03-27 14:15:05 +0100

    refactor sanity-utils

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,14 +1,10 @@
 import { createClient, groq } from "next-sanity";
 import { Project } from '@/types/Project'
+import clientConfig from "./config/client-config";
 
 export async function getProjects(): Promise<Project[]> {
-    const client = createClient({
-        projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-        dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-        apiVersion: "2025-03-04",
-    }) 
 
-    return client.fetch(
+    return createClient(clientConfig).fetch(
         groq`*[_type == "project"]{
         _id,
         _createdAt,
@@ -25,13 +21,7 @@ export async function getProjects(): Promise<Project[]> {
 }
 
 export async function getProject(slug: string): Promise<Project> {
-    const client = createClient({
-        projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-        dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-        apiVersion: "2025-03-04",
-    }) 
-
-    return client.fetch(
+    return createClient(clientConfig).fetch(
         groq`*[_type == "project" && slug.current == $slug][0]{
         _id,
         _createdAt,
