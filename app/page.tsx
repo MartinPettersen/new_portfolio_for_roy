13907dd630bc0bc5c9ed06288dde0fb6499b9036
MartinPettersen <searchingtheskyfor@gmail.com>
2025-03-31 10:21:22 +0200

    fix rounded corner bug

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
 
 
   return (
-<div className="w-screen rounded-tr-2xl rounded-br-xl rounded-bl-xl bg-[#E4D9C4] h-screen flex items-center justify-center">
-      <main className=" w-full flex items-center justify-center ">
+<div className="w-full rounded-tr-2xl rounded-br-xl rounded-bl-xl bg-[#E4D9C4] h-screen flex items-center justify-center">
+      <main className=" flex items-center justify-center ">
       <ProjectField />
       
       </main>
