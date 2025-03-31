cc1816551c5fee265a71f4e10546c2166b3915f2
MartinPettersen <searchingtheskyfor@gmail.com>
2025-03-31 10:25:56 +0200

    fix bug with background opacity

--- app/components/(projects)/ProjectDisplay.tsx
+++ app/components/(projects)/ProjectDisplay.tsx
@@ -12,7 +12,7 @@ const ProjectDisplay = ({ setToggleDisplay, project }: Props) => {
     return (
     <div className="">
       <div
-        className="w-screen left-0 top-0 h-screen bg-opacity-20 bg-slate-800 z-[100] fixed"
+        className="w-screen left-0 top-0 h-screen bg-slate-800/60 z-[100] fixed"
         onClick={() => setToggleDisplay(false)}
       ></div>
       <div className="z-[200] fixed left-0 top-[30vh] w-screen h-[50%]">
