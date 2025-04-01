4d56f3f31bb998f8138e7b19a0517eec7631c0ba
MartinPettersen <searchingtheskyfor@gmail.com>
2025-04-01 12:50:03 +0200

    fix left corner on about page

--- app/components/(navigation)/Navbar.tsx
+++ app/components/(navigation)/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       {pages.map((page) => (
         <Link href={`${page.url.toLowerCase()}`}
           key={page.name}
-          className={`${pathname === page.url? "w-[50%] h-24 shadow-custom z-[13]" : "w-[45%] h-20 z-[1] shadow-weak"}   flex items-center rounded-t-xl justify-center   font-medium  ${pathname === page.url? "bg-[#E4D9C4]" : "bg-[#B7AF9F]"}`}
+          className={`${pathname === page.url? "w-[50%] h-24 shadow-custom z-[13]" : "w-[45%] pb-4 -mb-4 h-24 z-[1] shadow-weak"}   flex items-center rounded-t-xl justify-center   font-medium  ${pathname === page.url? "bg-[#E8D5B0]" : "bg-[#9E9178]"}`}
         >
           <p className="text-lg ">
             {page.name}
