64e826dc589775ae971041fe0ee5123f3f1bdb5c
MartinPettersen <searchingtheskyfor@gmail.com>
2025-04-04 08:22:57 +0200

    add pushin to image

--- app/components/(about)/ImageContainer.tsx
+++ app/components/(about)/ImageContainer.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Image } from "next-sanity/image";
+import PushPin from './PushPin';
 
 type Props = {
     portrait: string,
@@ -8,9 +9,12 @@ type Props = {
 
 const ImageContainer = ({portrait, fullName}: Props) => {
   return (
-    <div className='w-[600px] flex items-center justify-center h-[600px] bg-[#F3E9D6]'>
+    <div className='w-[600px] flex relative items-center justify-center h-[600px] bg-[#F3E9D6]'>
+        <div className='top-[-20] left-[65%] z-[20] absolute'>
+          <PushPin />
+        </div>
         <Image
-            className="w-[548px] h-[521px] rotate-3"
+            className="w-[548px] h-[521px] z-[10] absolute rotate-3"
             src={portrait}
             alt={fullName || "Portrait Image"}
             width={2000}
---
+++ app/components/(about)/PushPin.tsx
@@ -0,0 +1,263 @@
+import React from "react";
+
+const PushPin = () => {
+  return (
+    <svg
+      width="126"
+      height="127"
+      viewBox="0 0 126 127"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_di_176_230)">
+        <path
+          d="M22.0001 111C20.5 109 24.3104 100.578 32.4708 86.5001L46.5002 95.9553C46.5002 95.9553 33.5619 107.701 26.5 113C22.28 116.167 23.5002 113 22.0001 111Z"
+          fill="#D9D9D9"
+        />
+      </g>
+      <g filter="url(#filter1_di_176_230)">
+        <ellipse
+          cx="52.4086"
+          cy="76.0492"
+          rx="40.7536"
+          ry="35.1912"
+          transform="rotate(27.547 52.4086 76.0492)"
+          fill="#F05D7E"
+        />
+      </g>
+      <g filter="url(#filter2_di_176_230)">
+        <path
+          d="M32.7131 52.971C26.7451 57 27.7719 75.4304 37.245 85.5C47.2449 96.1296 68.745 101.5 76.9469 91.3626L84.7449 67L98.2452 42.3021L74.9975 25L53.8553 40.5L32.7131 52.971Z"
+          fill="#F05D7E"
+        />
+      </g>
+      <g filter="url(#filter3_di_176_230)">
+        <ellipse
+          cx="88.3469"
+          cy="34.7876"
+          rx="30.3814"
+          ry="21.7984"
+          transform="rotate(32.8282 88.3469 34.7876)"
+          fill="#F05D7E"
+        />
+      </g>
+      <path
+        d="M15.811 106C17.7161 107.032 21.8155 109.822 22.973 112.724C24.1305 115.625 24.6395 118.744 24.7493 119.941"
+        stroke="black"
+        stroke-linecap="round"
+      />
+      <path
+        d="M34.1938 114.421C34.4068 114.597 34.722 114.566 34.8976 114.353C35.0733 114.14 35.043 113.825 34.83 113.649L34.1938 114.421ZM23.592 114.991C25.0234 114.724 27.3701 114.293 29.5473 114.087C30.6371 113.984 31.6671 113.939 32.5099 113.995C32.9311 114.022 33.293 114.074 33.5858 114.151C33.8834 114.23 34.0784 114.326 34.1938 114.421L34.83 113.649C34.5596 113.426 34.2106 113.282 33.8403 113.184C33.4653 113.086 33.0354 113.027 32.5754 112.997C31.6559 112.936 30.5658 112.987 29.4531 113.092C27.2254 113.303 24.8377 113.741 23.408 114.009L23.592 114.991Z"
+        fill="black"
+      />
+      <defs>
+        <filter
+          id="filter0_di_176_230"
+          x="21.6797"
+          y="86.5002"
+          width="25.8208"
+          height="27.8425"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="1" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_176_230"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_176_230"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="shape"
+            result="effect2_innerShadow_176_230"
+          />
+        </filter>
+        <filter
+          id="filter1_di_176_230"
+          x="12.769"
+          y="39.5916"
+          width="79.2788"
+          height="75.9153"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="3" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_176_230"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_176_230"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="2" dy="-1" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="shape"
+            result="effect2_innerShadow_176_230"
+          />
+        </filter>
+        <filter
+          id="filter2_di_176_230"
+          x="29"
+          y="25"
+          width="69.2451"
+          height="74.7654"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="3" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_176_230"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_176_230"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="5" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="shape"
+            result="effect2_innerShadow_176_230"
+          />
+        </filter>
+        <filter
+          id="filter3_di_176_230"
+          x="59.208"
+          y="10.1543"
+          width="57.2773"
+          height="51.2668"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="-1" dy="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_176_230"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_176_230"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="4" dy="-1" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="shape"
+            result="effect2_innerShadow_176_230"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
+
+export default PushPin;
