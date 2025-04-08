d3575171a2a09b3d61d5bb7754e9c1eb6ecff475
MartinPettersen <searchingtheskyfor@gmail.com>
2025-04-08 08:04:20 +0200

    add download button

--- app/components/(about)/ClipBoardButton.tsx
+++ app/components/(about)/ClipBoardButton.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const ClipBoardButton = ({text, label}: Props) => {
   return (
-    <p className="p-2 flex items-center justify-center hover:cursor-pointer bg-[#F3E9D6] shadow-custom" onClick={() => {navigator.clipboard.writeText(text)}}>{label}</p>
+    <p className="p-2 text-2xl flex items-center justify-center hover:cursor-pointer bg-[#F3E9D6] shadow-custom" onClick={() => {navigator.clipboard.writeText(text)}}>{label}</p>
 
   )
 }
--- app/components/(about)/DownloadButton.tsx
+++ app/components/(about)/DownloadButton.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Image from 'next/image'
 import React from 'react'
 
 type Props = {
@@ -15,7 +16,7 @@ const DownloadButton = ({label, cv}: Props) => {
       }
   }
   return (
-    <button onClick={() => downloadCV()} className=" p-2 hover:cursor-pointer bg-[#F3E9D6] shadow-custom">{label}</button>
+    <button onClick={() => downloadCV()} className=" p-2 text-2xl hover:cursor-pointer bg-[#F3E9D6] shadow-custom flex items-center justify-center gap-2">{label} <Image src="/DownloadIcon.svg" alt="Download" width={30} height={30} /></button>
 
   )
 }
