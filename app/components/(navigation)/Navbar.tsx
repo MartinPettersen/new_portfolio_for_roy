04de2dd4955c21c021d3febd24eac37843250697
MartinPettersen <searchingtheskyfor@gmail.com>
2025-04-08 09:02:55 +0200

    update navbar design

--- app/components/(navigation)/Navbar.tsx
+++ app/components/(navigation)/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       {pages.map((page) => (
         <Link href={`${page.url.toLowerCase()}`}
           key={page.name}
-          className={`${pathname === page.url? "w-[50%] h-24 shadow-custom z-[13]" : "w-[45%] pb-4 -mb-4 h-24 z-[1] shadow-weak"}   flex items-center rounded-t-xl justify-center   font-medium  ${pathname === page.url? "bg-[#E8D5B0]" : "bg-[#9E9178]"}`}
+          className={`${pathname === page.url? "w-[50%] h-24 shadow-nav  z-[13]" : "w-[45%] pb-4 -mb-4 h-[68px] z-[1] hover:w-[47%] hover:h-[72px]"}   flex items-center rounded-t-xl justify-center   font-medium  ${pathname === page.url? "bg-[#E8D5B0]" : "bg-[#9E9178]"}`}
         >
           <p className="text-lg font-space-mono">
             {page.name}
