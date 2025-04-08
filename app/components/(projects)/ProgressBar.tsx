7d5883ef80f8aabd1699f666ccad137269adea58
MartinPettersen <searchingtheskyfor@gmail.com>
2025-04-08 09:40:57 +0200

    add svg to progressbar

--- app/components/(projects)/ProgressBar.tsx
+++ app/components/(projects)/ProgressBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 type Props = {
     stageCompleted: number
@@ -8,13 +9,26 @@ const ProgressBar = ({stageCompleted}: Props) => {
   return (
     <div className='w-full flex flex-row h-2'>
       {stageCompleted === 1 ? 
-        <div style={{ width: `${33}%` }} className="bg-[#F05D7E]"></div>
+        <div style={{ width: `${33}%` }} className="">
+          <Image className="w-full h-full object-fill" src="/ProgressRed.svg" alt="progressbar" width={100} 
+            height={10} 
+            style={{ height: '100%', width: 'auto' }} />
+        </div>
       : null}
       {stageCompleted === 2 ? 
-        <div style={{ width: `${66}%` }} className="bg-[#FFAA75]"></div>
+        <div style={{ width: `${66}%` }} className="">
+        <Image className="w-full h-full object-fill" src="/ProgressOrange.svg" alt="progressbar" width={100} 
+            height={10} 
+            style={{ height: '100%', width: 'auto' }}/>
+      </div>
       : null}
       {stageCompleted === 3 ? 
-        <div style={{ width: `${100}%` }} className="bg-[#6A894B]"></div>
+        <div style={{ width: `${100}%` }}>
+        <Image className="w-full h-full object-fill" src="/ProgressGreen.svg" alt="progressbar" width={100} 
+            height={10} 
+            style={{ height: '100%', width: 'auto' }}/>
+          
+        </div>
       : null}
     </div>
   )
