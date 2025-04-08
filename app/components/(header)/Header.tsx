525ea7432443c65febcfd9aa11d1f33bf274ee6a
MartinPettersen <searchingtheskyfor@gmail.com>
2025-04-08 13:48:12 +0200

    add banner loading

--- app/components/(header)/Header.tsx
+++ app/components/(header)/Header.tsx
@@ -3,6 +3,11 @@ import HeaderBanner from "./HeaderBanner";
 
 export default async function Header() {
     const siteData = await getSiteData();
+
+    if (!siteData) {
+      return <div>Loading...</div>
+    }
+
   return (
     <header className="w-full flex flex-col">
       <HeaderBanner siteData={siteData} />
