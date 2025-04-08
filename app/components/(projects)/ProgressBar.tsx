54868b0be944b574382dfc233aec09ba8f78e4cb
MartinPettersen <searchingtheskyfor@gmail.com>
2025-04-08 10:03:06 +0200

    update button shadow

--- app/components/(projects)/ProgressBar.tsx
+++ app/components/(projects)/ProgressBar.tsx
@@ -11,21 +11,21 @@ const ProgressBar = ({stageCompleted}: Props) => {
       {stageCompleted === 1 ? 
         <div style={{ width: `${33}%` }} className="">
           <Image className="w-full h-full object-fill" src="/ProgressRed.svg" alt="progressbar" width={100} 
-            height={10} 
+            height={20} 
             style={{ height: '100%', width: 'auto' }} />
         </div>
       : null}
       {stageCompleted === 2 ? 
         <div style={{ width: `${66}%` }} className="">
         <Image className="w-full h-full object-fill" src="/ProgressOrange.svg" alt="progressbar" width={100} 
-            height={10} 
+            height={20} 
             style={{ height: '100%', width: 'auto' }}/>
       </div>
       : null}
       {stageCompleted === 3 ? 
         <div style={{ width: `${100}%` }}>
         <Image className="w-full h-full object-fill" src="/ProgressGreen.svg" alt="progressbar" width={100} 
-            height={10} 
+            height={20} 
             style={{ height: '100%', width: 'auto' }}/>
           
         </div>
