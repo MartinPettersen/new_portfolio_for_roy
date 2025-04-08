5251220121b8d123d9105cb42654b7d44b6ebad1
MartinPettersen <searchingtheskyfor@gmail.com>
2025-04-08 08:26:06 +0200

    add hover effects projects arrow buttons

--- app/components/(projects)/(project pagination)/ProjectButtonLeft.tsx
+++ app/components/(projects)/(project pagination)/ProjectButtonLeft.tsx
@@ -5,11 +5,25 @@ type Props = {
 };
 
 const ProjectButtonLeft = ({ action }: Props) => {
-  return <svg className={"svg-shadow"} onClick={() => action()}  width="72" height="81" viewBox="0 0 72 81" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M2.99999 45.6962C-1.00001 43.3867 -1 37.6133 3 35.3039L62.25 1.09585C66.25 -1.21355 71.25 1.6732 71.25 6.292L71.25 74.708C71.25 79.3268 66.25 82.2136 62.25 79.9042L2.99999 45.6962Z" fill="#8E9FB6"/>
-</svg>
+  return (
+    <div className="group inline-block">
 
-  
+    <svg
+      className={"svg-shadow hover:cursor-pointer transition-colors group-hover:scale-120"}
+      onClick={() => action()}
+      width="72"
+      height="81"
+      viewBox="0 0 72 81"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      >
+      <path
+        d="M2.99999 45.6962C-1.00001 43.3867 -1 37.6133 3 35.3039L62.25 1.09585C66.25 -1.21355 71.25 1.6732 71.25 6.292L71.25 74.708C71.25 79.3268 66.25 82.2136 62.25 79.9042L2.99999 45.6962Z"
+        className="transition-colors fill-[#8E9FB6] group-hover:fill-[#264773]"
+        />
+    </svg>
+        </div>
+  );
 };
 
 export default ProjectButtonLeft;
--- app/components/(projects)/(project pagination)/ProjectButtonRight.tsx
+++ app/components/(projects)/(project pagination)/ProjectButtonRight.tsx
@@ -5,9 +5,13 @@ type Props = {
 };
 
 const ProjectButtonRight = ({ action }: Props) => {
-  return <svg className={" rotate-180 svg-shadow "} onClick={() => action()}  width="72" height="81" viewBox="0 0 72 81" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M2.99999 45.6962C-1.00001 43.3867 -1 37.6133 3 35.3039L62.25 1.09585C66.25 -1.21355 71.25 1.6732 71.25 6.292L71.25 74.708C71.25 79.3268 66.25 82.2136 62.25 79.9042L2.99999 45.6962Z" fill="#8E9FB6"/>
-  </svg>
+  return <div className="group inline-block">
+    <svg className={" svg-shadow hover:cursor-pointer transition-colors group-hover:scale-120"} onClick={() => action()} width="72" height="81" viewBox="0 0 72 81" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M69 35.3038C73 37.6133 73 43.3867 69 45.6961L9.74998 79.9041C5.74998 82.2135 0.749988 79.3268 0.749989 74.708L0.749992 6.29198C0.749992 1.67318 5.74999 -1.21356 9.74999 1.09584L69 35.3038Z" 
+className="transition-colors fill-[#8E9FB6] group-hover:fill-[#264773]"/>
+</svg>
+
+  </div>
 };
 
 export default ProjectButtonRight;
