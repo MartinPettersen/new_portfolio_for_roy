5ccb074d2730e8ef63dc35c3c5f6a5e391c98520
MartinPettersen <searchingtheskyfor@gmail.com>
2025-04-09 11:45:46 +0200

    update hobby

--- app/components/(about)/(hobby)/QuoteCard.tsx
+++ app/components/(about)/(hobby)/QuoteCard.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
+import { PortableText } from "@portabletext/react";
+import { Hobby } from "@/types/Hobby";
 
 type Props = {
-    label: string
-}
+  hobby: Hobby;
+};
 
-const QuoteCard = ({label}: Props) => {
+const QuoteCard = ({ hobby }: Props) => {
   return (
-    <div className='flex w-[384px] text-5xl font-permanent-marker h-[388px] items-center justify-center'>{label}</div>
-  )
-}
+    <div className="flex w-[384px] text-5xl font-permanent-marker h-[388px] items-center justify-center">
+      {hobby.description!.map(
+        (content, i: number) =>
+          content && <PortableText key={i} value={content} />
+      )}
+    </div>
+  );
+};
 
-export default QuoteCard
+export default QuoteCard;
