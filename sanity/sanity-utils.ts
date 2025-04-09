c6a55c1d304d531b92be12a92bbc1ef8a56ab110
MartinPettersen <searchingtheskyfor@gmail.com>
2025-04-09 11:33:22 +0200

    sanity fetch fix

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -4,34 +4,8 @@ import clientConfig from "./config/client-config";
 
 export async function getProjects(): Promise<Project[]> {
 
-    return createClient(clientConfig).fetch(
-        groq`*[_type == "project"]{
-        _id,
-        _createdAt,
-        title,
-        "slug": slug.current,
-        "coverimage": coverimage.asset->url,
-        url,
-        content[]{
-          _type == 'contentBlock' => {
-            content,
-            image{
-              "asset": asset->url, // Fetch the URL here
-              alt
-            }
-          },
-          _type != 'contentBlock' => @ // Keep other block types as they are
-        },
-        excerpt,
-        progress,
-        tags
-        }`
-    )
-}
-
-export async function getProject(slug: string): Promise<Project> {
-    return createClient(clientConfig).fetch(
-        groq`*[_type == "project" && slug.current == $slug][0]{
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "project"]{
         _id,
         _createdAt,
         title,
@@ -52,13 +26,46 @@ export async function getProject(slug: string): Promise<Project> {
         progress,
         tags
         }`,
-        { slug }
-    )
+        {},
+        {
+          cache: 'no-store'
+        }
+  )
+}
+
+export async function getProject(slug: string): Promise<Project> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "project" && slug.current == $slug][0]{
+        _id,
+        _createdAt,
+        title,
+        "slug": slug.current,
+        "coverimage": coverimage.asset->url,
+        url,
+        content[]{
+          _type == 'contentBlock' => {
+            content,
+            image{
+              "asset": asset->url, // Fetch the URL here
+              alt
+            }
+          },
+          _type != 'contentBlock' => @ // Keep other block types as they are
+        },
+        excerpt,
+        progress,
+        tags
+        }`,
+    { slug },
+    {
+      cache: 'no-store'
+    }
+  )
 }
 
 export async function getAbout() {
-    const data = await createClient(clientConfig).fetch(
-        groq`*[_type == "about"]{
+  const data = await createClient(clientConfig).fetch(
+    groq`*[_type == "about"]{
         _id,
         _createdAt,
         fullName,
@@ -73,23 +80,27 @@ export async function getAbout() {
         "image": image.asset->url,
         "alt": image.alt
       }
-        }`
-    )
-    return data[0] || null;
+        }`,
+        {},
+        {
+          cache: 'no-store'
+        }
+  )
+  return data[0] || null;
 }
 
 export async function getSiteData() {
   const data = await createClient(clientConfig).fetch(
-      groq`*[_type == "sitedata"]{
+    groq`*[_type == "sitedata"]{
       _id,
       _createdAt,
       quotes,
       bannerquote
       }`,
-      {},
-      {
-        cache: 'no-store'
-      }
+    {},
+    {
+      cache: 'no-store'
+    }
   )
   return data[0] || null;
 }
