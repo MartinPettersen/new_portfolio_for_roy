34f0c60ca16654aebd158c194900cdfd1739c19d
MartinPettersen <searchingtheskyfor@gmail.com>
2025-04-09 10:14:48 +0200

    test

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
  
 export default function NotFound() {
   return (
-    <div className="">
+    <div className="flex flex-col pt-8 rounded-t-[18px] rounded-br-[18px] rounded-bl-[18px] shadow-nav z-[12] relative bg-[#E8D5B0] items-center justify-center">
       <h2 className="">Page Not Found</h2>
       <p className="">Could not find the requested resource</p>
       <Link href="/" className="">
