9b47ec4c4eee08f3008b0bb47901d9efd8c2d465
MartinPettersen <searchingtheskyfor@gmail.com>
2025-04-10 13:44:20 +0200

    add hover label on copy

--- app/components/(about)/ClipBoardButton.tsx
+++ app/components/(about)/ClipBoardButton.tsx
@@ -8,7 +8,13 @@ type Props = {
 
 const ClipBoardButton = ({text, label}: Props) => {
   return (
-    <p className="p-2 text-2xl flex items-center justify-center hover:cursor-pointer bg-[#F3E9D6]  hover:bg-[#9E9178] hover:font-bold hover:text-white inner-shadow-button" onClick={() => {navigator.clipboard.writeText(text)}}>{label}</p>
+    <div className='group flex items-center justify-end'>
+
+    <p className="p-2 z-1 w-full text-2xl flex items-center justify-center hover:cursor-pointer bg-[#F3E9D6]  hover:bg-[#9E9178] hover:font-bold hover:text-white inner-shadow-button" onClick={() => {navigator.clipboard.writeText(text)}}>{label}</p>
+    <div className='z-2 absolute hidden group-hover:flex px-2 bg-[#F3E9D6] border-[1px]'>
+      Copy
+    </div>
+    </div>
 
   )
 }
