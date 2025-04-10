ea1e2d31dd3cdc9fc009a57aa7d7ac07afdb0ffb
MartinPettersen <searchingtheskyfor@gmail.com>
2025-04-10 09:22:45 +0200

    adjust navbar for mobile

--- app/components/(navigation)/Navbar.tsx
+++ app/components/(navigation)/Navbar.tsx
@@ -8,14 +8,14 @@ const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="w-[50%] flex justify-start items-end ">
+    <nav className="md:w-[50%] flex justify-start items-end ">
       
       {pages.map((page) => (
         <Link href={`${page.url.toLowerCase()}`}
           key={page.name}
-          className={`${pathname === page.url? "w-[50%] h-[76]  shadow-nav  z-[13]" : "w-[45%] pb-4 -mb-4 h-[80px] z-[1] "}   flex items-center rounded-t-xl justify-center   font-medium  ${pathname === page.url? "bg-[#E8D5B0]" : "bg-[#9E9178] hover:bg-[#C3B394]"}`}
+          className={`${pathname === page.url? "w-[178px] md:w-[50%] h-[76]  shadow-nav  z-[13]" : "w-[159px] md:w-[45%] pb-4 -mb-4 h-[80px] z-[1] "}   flex items-center rounded-t-xl justify-center   font-medium  ${pathname === page.url? "bg-[#E8D5B0]" : "bg-[#9E9178] hover:bg-[#C3B394]"}`}
         >
-          <p className="text-lg font-space-mono">
+          <p className="text-[24px] md:text-[36px] font-space-mono">
             {page.name}
           </p>
         </Link>
