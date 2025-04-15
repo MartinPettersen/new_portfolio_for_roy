a18c35244c28a647ba3f9d6713924df72c3b8cd3
MartinPettersen <searchingtheskyfor@gmail.com>
2025-04-15 08:50:54 +0200

    first draft framer motion

--- app/components/(about)/ImageContainer.tsx
+++ app/components/(about)/ImageContainer.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import { Image } from "next-sanity/image";
 import PushPin from './PushPin';
+import { motion } from 'framer-motion'
 
 type Props = {
     portrait: string,
@@ -8,18 +10,68 @@ type Props = {
 }
 
 const ImageContainer = ({portrait, fullName}: Props) => {
+
+  const [isFalling, setIsFalling] = useState(false)
+
+
+  const handlePushPinClick = () => {
+    setIsFalling(true)
+  }
+
+  const offScreenDistance = 2000
+
   return (
     <div className='w-[185px] h-[185px] md:w-[600px] flex relative items-center justify-center md:h-[600px] bg-[#F3E9D6] inner-shadow-image-card'>
-        <div className='top-[0] md:top-[-20] left-[65%] z-[20] absolute '>
+        <motion.div onClick={handlePushPinClick} 
+        className={`top-[0] md:top-[-20] left-[65%] z-[20] absolute custom-mouse-pointer
+             `}
+             initial={{ x: 0, y: 0, rotate: 0 }}
+          animate={isFalling ? { 
+            x: 400,
+            y: offScreenDistance,
+            rotate: 720, 
+          } : {}}
+          transition={{
+            type: "keyframes",
+            duration: 2,
+            ease: "easeIn",
+            rotate: {
+              duration: 2,
+              repeat: 0,
+              ease: "linear" 
+            }
+          }}
+             >
           <PushPin />
-        </div>
+        </motion.div>
+        <motion.div
+        className='w-[90%] h-[90%] md:w-[500px] md:h-[500px] z-[10] absolute'
+        initial={{ rotate: 3 }}
+          animate={isFalling ? { 
+            x: -200,
+            y: offScreenDistance,
+            rotate: -220,
+          } : {}}
+          transition={{
+            type: "keyframes",
+            duration: 2.5,
+            ease: "easeIn",
+            rotate: {
+              duration: 2.5,
+              repeat: 0,
+              ease: "linear" 
+            }
+          }}>
+
         <Image
-            className="w-[90%] h-[90%] md:w-[500px] md:h-[500px] z-[10] absolute rotate-3 shadow-project-image"
+            className="w-full h-fullshadow-project-image"
+            
             src={portrait}
             alt={fullName || "Portrait Image"}
             width={500}
             height={500}
-          />
+            />
+            </motion.div>
     </div>
   )
 }
