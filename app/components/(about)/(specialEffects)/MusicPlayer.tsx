89479c3e59167c5356d4a15a91474296e13e4121
MartinPettersen <searchingtheskyfor@gmail.com>
2025-04-22 12:56:10 +0200

    change custom mouse pointer

--- app/components/(about)/(specialEffects)/MusicPlayer.tsx
+++ app/components/(about)/(specialEffects)/MusicPlayer.tsx
@@ -1,17 +1,17 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect } from "react";
 import Image from "next/image";
 
 export default function MusicPlayer() {
   const audioRef = useRef<HTMLAudioElement | null>(null);
 
   useEffect(() => {
-    audioRef.current = new Audio('/pinkfrogsong.mp3');
+    audioRef.current = new Audio("/pinkfrogsong.mp3");
   }, []);
 
   const handlePlay = () => {
     if (audioRef.current) {
       audioRef.current.play().catch((error) => {
-        console.error('Error playing audio:', error);
+        console.error("Error playing audio:", error);
       });
     }
   };
@@ -19,13 +19,13 @@ export default function MusicPlayer() {
   return (
     <div>
       <button onClick={handlePlay}>
-         <Image
-                className="w-full h-full shadow-project-image custom-mouse-pointer"
-                src="/pinkFrog.png"
-                alt="Pink frog"
-                width={400}
-                height={400}
-                />
+        <Image
+          className="w-full h-full shadow-project-image custom-mouse-pointer"
+          src="/pinkFrog.png"
+          alt="Pink frog"
+          width={400}
+          height={400}
+        />
       </button>
     </div>
   );
