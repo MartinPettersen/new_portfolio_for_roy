cd07061af6afe90dd2df02f15bd7b489f2d91e59
MartinPettersen <searchingtheskyfor@gmail.com>
2025-04-22 11:45:15 +0200

    fix hobby card image

--- app/components/(about)/(hobby)/HobbyCard.tsx
+++ app/components/(about)/(hobby)/HobbyCard.tsx
@@ -12,21 +12,23 @@ const HobbyCard = ({ hobby }: Props) => {
 
   useEffect(() => {
     let num = Math.floor(Math.random() * 2) + 1;
-    num *= Math.round(Math.random()) ? 1 : -1
+    num *= Math.round(Math.random()) ? 1 : -1;
     setRandomNumber(num);
   }, []);
 
   return (
     <div className="flex w-[163px]  h-[165px] md:w-[384px]  md:h-[388px] bg-[#F3E9D6] items-center justify-center inner-shadow-image-card">
-      {hobby.image && (randomNumber > -100) ? (
-        <Image
-          className={`shadow-project-image object-cover`}
-          style={{ transform: `rotate(${randomNumber}deg)` }}
-          src={hobby.image}
-          alt="hobby image"
-          width={315}
-          height={315}
-        />
+      {hobby.image && randomNumber > -100 ? (
+        <div className="flex items-center relative justify-center w-[94%] h-[94%]">
+          <Image
+            className={`shadow-project-image  object-contain `}
+            style={{ transform: `rotate(${randomNumber}deg)` }}
+            fill={true}
+            src={hobby.image}
+            alt="hobby image"
+            sizes=""
+          />
+        </div>
       ) : null}
     </div>
   );
