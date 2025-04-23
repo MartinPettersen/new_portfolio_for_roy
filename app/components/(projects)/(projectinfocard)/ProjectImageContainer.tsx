7a60f0ded9b40e9da890dd83a36e505b1ae8675c
MartinPettersen <searchingtheskyfor@gmail.com>
2025-04-23 13:48:51 +0200

    add texture to project info card

--- app/components/(projects)/(projectinfocard)/ProjectImageContainer.tsx
+++ app/components/(projects)/(projectinfocard)/ProjectImageContainer.tsx
@@ -10,8 +10,9 @@ type Props = {
 const ProjectImageContainer = ({ project, projectIndex }: Props) => {
   return (
     <div className=" md:pt-0 flex flex-none w-[90%] items-center justify-center">
-      <div className="flex md:mr-0 md:mt-0 w-[96%] md:w-full  h-[80%] md:h-full bg-[#f3e9d6] items-center justify-center inner-shadow-image-card">
+      <div className={`flex md:mr-0 md:mt-0 w-[96%] md:w-full overflow-hidden h-[80%] md:h-full bg-[#f3e9d6] bg-svg-pattern bg-no-repeat bg-center bg-cover  items-center justify-center inner-shadow-image-card`}>
         <div className=" w-full m-4 h-full">
+          
           {project.content &&
             project.content.length > 0 &&
             project.content[projectIndex].image && (
