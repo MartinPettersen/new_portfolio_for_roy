a5349b31b7ed2e32deb9e0bcc52b6a9aed85486d
MartinPettersen <searchingtheskyfor@gmail.com>
2025-04-23 12:06:12 +0200

    add shadow to red button

--- app/components/(about)/(specialEffects)/BigRedButton.tsx
+++ app/components/(about)/(specialEffects)/BigRedButton.tsx
@@ -1,11 +1,17 @@
 "use client";
+import Image from "next/image";
 import React from "react";
 
 const BigRedButton = () => {
   return (
-    <div className="group custom-mouse-pointer">
-      <div className="hidden group-hover:block">BigRedButton</div>
-      <div className="group-hover:hidden" >BigBlueButton</div>
+    <div className="group custom-mouse-pointer ">
+      <div className=" z-[10]">
+      <Image src={"/WarningButton.png"} className=" red-button-shadow" alt="red warning button" fill={true} />
+      </div>
+      <div className="group-hover:hidden z-[20] " >
+        <Image src={"/DoNotPressButton.png"} className="" alt="red do not press button" fill={true} />
+
+      </div>
     </div>
   );
 };
