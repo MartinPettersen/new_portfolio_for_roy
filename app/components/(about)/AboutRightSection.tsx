ef5b2d7bd227e7dc4256d5201b716c7a97c0d8df
MartinPettersen <searchingtheskyfor@gmail.com>
2025-05-12 11:33:46 +0200

    fixed padding top h2

--- app/components/(about)/AboutRightSection.tsx
+++ app/components/(about)/AboutRightSection.tsx
@@ -9,15 +9,15 @@ type Props = {
 const AboutRightSection = ({ aboutData }: Props) => {
   return (
     <div className="w-full md:w-[50%] flex flex-col items-center md:items-start pr-6 md:pr-0 md:px-8 h-full justify-center m-4">
-      <h1 className="font-bold hidden md:flex w-full items-center justify-start text-6xl pb-8 font-libre-baskerville">
+      <h1 className="font-bold hidden md:flex w-full items-center justify-start text-6xl  font-libre-baskerville">
         {aboutData.fullName}
       </h1>
-      <div className="font-extralight border-t-2 border-[#181818]/20 md:border-0 w-[94vw] md:w-full text-sm md:text-2xl paragraph">
+      <article id="about" className="font-extralight border-t-2 border-[#181818]/20 md:border-0 w-[94vw] md:w-full text-sm md:text-2xl paragraph">
         {aboutData.about.map(
           (content, i: number) =>
             content && <PortableText key={i} value={content} />
         )}
-      </div>
+      </article>
     </div>
   );
 };
